b6ea7741f6fabc1282d5813b4b5f4f2f06dd2762
Ignacio Carmach <icarmach@gail.com>
2019-04-12 09:44:20 -0400

    Add Office 365 unlicensed users to a group

---
+++ Add Office 365 unlicensed users to a group/AddO365UnlicensedUsersToAGroup.ps1
@@ -0,0 +1,20 @@
+Install-Module MSOnline
+Import-Module MSOnline
+
+Connect-MsolService
+
+$GroupName = "Test"
+$GroupInfo = Get-MsolGroup | Where-Object {$_.DisplayName -eq $GroupName}
+
+$FilePath = "C:\O365UnlicensedUsers.csv"
+
+Get-MsolUser -All -UnlicensedUsersOnly | Select UserPrincipalName | Export-CSV $FilePath -NoTypeInformation
+
+Import-CSV -Path $FilePath | ForEach { 
+	$UPN = $_.UserPrincipalName
+	$Users = Get-MsolUser -UserPrincipalName $UPN
+
+	$Users | ForEach {Add-MsolGroupMember -GroupObjectId $GroupInfo.ObjectID -GroupMemberObjectId $Users.ObjectID -GroupMemberType User}
+}
+
+rm C:\O365UnlicensedUsers.csv
